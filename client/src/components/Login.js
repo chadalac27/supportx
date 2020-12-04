fbccd61e3f2e688350f8b04efab69dd841297497
Sebastian Brear <ssbrear@ucdavis.edu>
2020-12-03 21:44:14 -0800

    Setup routing to begin working on login page

---
+++ client/src/components/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  mainContainer: { backgroundColor: "red" },
+});
+
+const Login = () => {
+  const classes = useStyles();
+  return (
+      <div>
+          
+      </div>
+  );
+};
+
+export default Login;
