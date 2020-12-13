323faa8f85f45168d21ba974a578d2beb370b379
John Baxter <jbaxter@a-zdarts.com>
2020-12-13 13:12:03 -0800

    added channel to ticket model, updated the ticket seed to include channel, added more tickets to seed

--- scripts/seedTicket.js
+++ scripts/seedTicket.js
@@ -26,7 +26,37 @@ function StartSeed() {
           senderName: "Customer",
         },
       ],
+      channel: "General, Processing, Shipping"
     },
+    {
+      agents: [],
+      companyID: "5fd64a7461aafa52a4421961",
+      severity: 8,
+      title: "My order is stuck",
+      messages: [
+        {
+          message: "Yo... i placed this order for your super weedmates special and the system is sitting here spinning.. whats up?",
+          timeStamp: new Date(),
+          senderName: "Customer",
+        },
+      ],
+      channel: "Processing"
+    },
+    {
+      agents: [],
+      companyID: "5fd64a7461aafa52a4421961",
+      severity: 2,
+      title: "Can you tell me my tracking number?",
+      messages: [
+        {
+          message: "I made a small order and have not recieved a tracking number, can you please send it to me?",
+          timeStamp: new Date(),
+          senderName: "Customer",
+        },
+      ],
+      channel: "Shipping"
+    },
+    
   ];
 
   let myMan;
@@ -41,6 +71,35 @@ function StartSeed() {
         timeStamp: new Date(),
         senderName: myMan.username,
       });
+      ticketSeed[1].agents.push({agentID:myMan._id });
+      //ticketSeed[0].messages[0].senderName = "Customer";
+      ticketSeed[1].messages.push({
+        message: "Oh! im sorry to hear you are having problems.  Please try holding ALT and pressing F4 at the same time. Your problem should be resolved ",
+        timeStamp: new Date(),
+        senderName: myMan.username,
+      });
+      ticketSeed[2].agents.push({agentID:dbUser[1]._id });
+      //ticketSeed[0].messages[0].senderName = "Customer";
+      ticketSeed[2].messages.push({
+        message: "What is your order number?",
+        timeStamp: new Date(),
+        senderName: myMan.username,
+      });
+      ticketSeed[2].messages.push({
+        message: "My order number is 559137",
+        timeStamp: new Date(),
+        senderName: "Customer",
+      });
+      ticketSeed[2].messages.push({
+        message: "I see it!  Your tracking number is #2345fd456ssY",
+        timeStamp: new Date(),
+        senderName: myMan.username,
+      });
+      ticketSeed[2].messages.push({
+        message: "Sweet Thank you!",
+        timeStamp: new Date(),
+        senderName: "Customer",
+      });
       UpdateDB();
     })
     .catch((err) => {
@@ -50,6 +109,8 @@ function StartSeed() {
   function UpdateDB() {
     db.Ticket.remove({})
       .then(() => db.Ticket.create(ticketSeed[0]))
+      .then(() => db.Ticket.create(ticketSeed[1]))
+      .then(() => db.Ticket.create(ticketSeed[2]))
       .then((data) => {
         process.exit(0);
       })
