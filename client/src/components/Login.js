e7e56784c37cca551c98fe492392719097f74757
John Baxter <jbaxter@a-zdarts.com>
2020-12-13 21:01:47 -0800

    Added more to authentication on react

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,31 +1,67 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import { Link } from "react-router-dom";
+import {AuthContext} from '../Context/AuthContext';
+import AuthService from '../utils/API';
 
 const Login = () => {
+
+  const [agent, setAgent] = useState({username: "", password: ""});
+  const [message, setMessage] = useState(null);
+  const authContext = useContext(AuthContext);
+
+
+  const onChange = e => {
+    e.preventDefault();
+    setAgent({...agent, [e.target.name] : e.target.value})
+    console.log(agent);
+  }
+
+  const onSubmit = e =>{
+    e.preventDefault();
+    AuthService.Login(agent).then(data => {
+      const {isAuthenticated, agent, message} = data;
+      if(isAuthenticated){
+        authContext.setAgent(agent);
+        authContext.setIsAuthenticated(isAuthenticated);
+      }
+      else{
+        setMessage(message);
+      }
+    })
+    console.log(e.target);
+  }
+
+  
+
+
   return (
-    <div class="login-wrapper">
-      <div class="title">SupportX</div>
-      <div class="formStyle">
-        <form class="login-form" action="#/dashboard">
+    <div className="login-wrapper">
+      <div className="title">SupportX</div>
+      <div className="formStyle">
+        <form className="login-form" onSubmit={onSubmit}>
           <label htmlFor="login-email">Email: </label>
           <input
             id="login-email"
             type="email"
-            class="inputStyle"
+            className="inputStyle"
             name="email"
+            onChange={onChange}
+            placeholder="Enter Username"
           />
           <label htmlFor="login-pw">Password: </label>
           <input
             id="login-pw"
             type="password"
-            class="inputStyle"
+            className="inputStyle"
             name="password"
+            onChange={onChange}
           />
           <Link to="/signup">Don't have an account?</Link>
-          <button type="submit" class="buttonStyle">
+          <button type="submit" className="buttonStyle">
             Log In
           </button>
         </form>
+        {/* {message ? <Message message={message}/> : null} */}
       </div>
     </div>
   );
