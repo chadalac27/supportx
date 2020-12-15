8d4e079718b001921bbb1c92bf1ff280f2b6eef7
John Baxter <jbaxter@a-zdarts.com>
2020-12-15 14:53:14 -0800

    Added more info to the seeds so it seems more realistic

--- scripts/agentSeed.js
+++ scripts/agentSeed.js
@@ -13,7 +13,7 @@ const agentSeed = [
     lastName: "Mc Testerson",
     emailAddress: "dan@mochi.com",
     avatarURL:
-      "https://image.shutterstock.com/image-vector/mochi-icon-wagashi-japanese-rice-260nw-459417508.jpg",
+      "https://us.123rf.com/450wm/fizkes/fizkes2007/fizkes200701793/152407909-profile-picture-of-smiling-young-caucasian-man-in-glasses-show-optimism-positive-and-motivation-head.jpg?ver=6",
   },
   {
     username: "mochi_sarah",
@@ -22,16 +22,16 @@ const agentSeed = [
     lastName: "Mc Testerson",
     emailAddress: "sarah@mochi.com",
     avatarURL:
-      "https://image.shutterstock.com/image-vector/mochi-icon-wagashi-japanese-rice-260nw-459417508.jpg",
+      "https://images.pexels.com/photos/1310522/pexels-photo-1310522.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
   },
   {
     username: "iryna",
     firstName: "Iryna",
     password: "password1",
-    lastName: "Mc Testerson",
+    lastName: "Grom",
     emailAddress: "iryna@nike.com",
     avatarURL:
-      "https://cdn.iconscout.com/icon/free/png-512/nike-1-202653.png",
+      "https://media-exp1.licdn.com/dms/image/C4E03AQEDnP5GIHWKKQ/profile-displayphoto-shrink_200_200/0/1517241722381?e=1613606400&v=beta&t=16F_we8beC1eSkPMc09XBy4dfVzoZ1KgbO8vt3Yfavc",
   },
   {
     username: "jbaxter",
@@ -42,13 +42,33 @@ const agentSeed = [
     avatarURL:
       "https://avatars3.githubusercontent.com/u/67088604?s=460&v=4",
   },
+  {
+    username: "sbrear",
+    firstName: "Sebastian",
+    password: "password1",
+    lastName: "Brear",
+    emailAddress: "sbrear@top-dps.com",
+    avatarURL:
+      "https://avatars1.githubusercontent.com/u/64553379?s=460&u=153d012bdd9372bf1480375ad96cc17a12167dfa&v=4",
+  },
+  {
+    username: "ccoleman",
+    firstName: "Chad",
+    password: "password1",
+    lastName: "Coleman",
+    emailAddress: "chadalac@weedmates.com",
+    avatarURL:
+      "https://avatars0.githubusercontent.com/u/12057073?s=460&u=1d9ec302c8301d0d46d7ff11aca5b94eaca851e8&v=4",
+  },
 ];
 
 db.Agent.remove({})
-  .then(() => db.Agent.create(agentSeed[0]))
-  .then(() => db.Agent.create(agentSeed[1]))
-  .then(() => db.Agent.create(agentSeed[2]))
-  .then(() => db.Agent.create(agentSeed[3]))
+  .then(() => db.Agent.create(agentSeed[0]))  //mochi_dan
+  .then(() => db.Agent.create(agentSeed[1]))  //mochi_sarah
+  .then(() => db.Agent.create(agentSeed[2]))  //iryna
+  .then(() => db.Agent.create(agentSeed[3]))  //jbaxter
+  .then(() => db.Agent.create(agentSeed[4]))  //sbrear
+  .then(() => db.Agent.create(agentSeed[5]))  //ccoleman
   .then((data) => {
     //console.log(data.result.n + " records inserted!");
     process.exit(0);
